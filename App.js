0c2401222e1706eef54d850392a61a28c94fdef6
Aavishkar04 <101863152+Aavishkar04@users.noreply.github.com>
2025-01-06 13:21:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <a href="#" className="block text-left max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow text-black ">
+
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-black  ">Noteworthy technology acquisitions 2021</h5>
+        <p className="font-normal text-slate-700 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      </a>
+
+    </div>
+  );
+}
+
+export default App;
